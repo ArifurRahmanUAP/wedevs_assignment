264206448ba4b2f8c787c8290deca1c341fb3fe4
Md. Arifur Rahman <uap.arifur@gmail.com>
2024-06-07 21:55:10 +0600

    LogOut added

--- lib/src/features/ProfileScreen/ui/screens/profile_screen.dart
+++ lib/src/features/ProfileScreen/ui/screens/profile_screen.dart
@@ -2,12 +2,17 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:wedevs_assignment/main.dart';
 import 'package:wedevs_assignment/src/core/extensions.dart';
+import 'package:wedevs_assignment/src/core/routes/router.dart';
 import 'package:wedevs_assignment/src/core/utilities/assets_path.dart';
-import 'package:wedevs_assignment/src/core/utilities/constants.dart';
+import 'package:wedevs_assignment/src/features/home_screen/ui/controller/home_screen_controller.dart';
+import 'package:wedevs_assignment/src/features/login_screen/ui/controller/login_controller.dart';
+import 'package:wedevs_assignment/src/features/signup_screen/ui/controller/signup_screen_controller.dart';
 
 import '../../../../core/common_method/common_method.dart';
 import '../../../../core/di/app_component.dart';
+import '../../../../core/routes/route_name.dart';
 import '../controller/profile_screen_controller.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -60,55 +65,70 @@ class ProfileScreen extends StatelessWidget {
                               Radius.circular(150),
                             ),
                             child: Obx(
-                                  () =>
-                                  Container(
-                                    height: 115,
-                                    width: 115,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: profileScreenController
-                                                .profileModel.value.id !=
+                              () => Container(
+                                height: 115,
+                                width: 115,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: profileScreenController
+                                                    .profileModel.value.id !=
                                                 null
-                                                ? NetworkImage(
+                                            ? NetworkImage(
                                                 profileScreenController
                                                     .profileModel
                                                     .value
                                                     .avatarUrls!["96"]!)
-                                                : const AssetImage(
-                                                "assets/image_not_found.jpg")
-                                            as ImageProvider,
-                                            fit: BoxFit.fill)),
-                                  ),
+                                            : const AssetImage(
+                                                    "assets/image_not_found.jpg")
+                                                as ImageProvider,
+                                        fit: BoxFit.fill)),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                     30.ph,
-                    Obx(
-                          () =>
-                          Text(
-                            profileScreenController.profileModel.value.name ?? "",
-                            style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontFamily: "Roboto",
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.bold),
-                          ),
-                    ),
-                    Obx(
-                          () =>
-                          Text(
-                            profileScreenController.profileModel.value.email ??
-                                "",
-                            style: const TextStyle(
-                                color: Color(0xFF535353),
-                                fontSize: 16,
-                                fontFamily: "Lato",
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.normal),
-                          ),
+                    Obx(() => Text(
+                          profileScreenController.profileModel.value.name ?? "",
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontFamily: "Roboto",
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.bold),
+                        )),
+                    Obx(() => Text(
+                          profileScreenController.profileModel.value.email ??
+                              "",
+                          style: const TextStyle(
+                              color: Color(0xFF535353),
+                              fontSize: 16,
+                              fontFamily: "Lato",
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.normal),
+                        )),
+                    5.ph,
+                    GestureDetector(
+                      onTap: () {
+                        session.logout().then((value) {
+                          Get.delete<HomeScreenController>();
+                          Get.delete<ProfileScreenController>();
+                          Get.delete<HomeScreenController>();
+                          Get.delete<SignupScreenController>();
+                          Get.delete<LoginController>();
+                          return RouteGenerator.pushNamedAndRemoveAll(
+                              context, Routes.splashScreenRouteName);
+                        });
+                      },
+                      child: const Text(
+                        "Log Out",
+                        style: TextStyle(
+                            color: Colors.orange,
+                            fontSize: 16,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                     25.ph,
                     Card(
@@ -129,8 +149,8 @@ class ProfileScreen extends StatelessWidget {
                                   isEnable: true,
                                 ),
                                 Container(
-                                  margin:
-                                  const EdgeInsets.symmetric(horizontal: 20),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 20),
                                   height: 2,
                                   color: const Color(0xffe1e4eb),
                                 ),
@@ -141,8 +161,8 @@ class ProfileScreen extends StatelessWidget {
                                   isEnable: false,
                                 ),
                                 Container(
-                                  margin:
-                                  const EdgeInsets.symmetric(horizontal: 20),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 20),
                                   height: 2,
                                   color: const Color(0xffe1e4eb),
                                 ),
@@ -153,8 +173,8 @@ class ProfileScreen extends StatelessWidget {
                                   isEnable: false,
                                 ),
                                 Container(
-                                  margin:
-                                  const EdgeInsets.symmetric(horizontal: 20),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 20),
                                   height: 2,
                                   color: const Color(0xffe1e4eb),
                                 ),
@@ -179,15 +199,11 @@ class ProfileScreen extends StatelessWidget {
                   child: Center(
                     child: Container(
                         padding: const EdgeInsets.all(10),
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width * .9,
+                        width: MediaQuery.of(context).size.width * .9,
                         height: 100,
                         decoration: BoxDecoration(
                             color: Colors.grey,
-                            borderRadius:
-                            BorderRadius.circular(10)),
+                            borderRadius: BorderRadius.circular(10)),
                         child: Row(
                           mainAxisSize: MainAxisSize.max,
                           crossAxisAlignment: CrossAxisAlignment.center,
@@ -271,10 +287,11 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 
-  Widget expandedWidgets({required children,
-    required String title,
-    required String icon,
-    required bool isEnable}) {
+  Widget expandedWidgets(
+      {required children,
+      required String title,
+      required String icon,
+      required bool isEnable}) {
     return Column(
       children: [
         ExpansionTile(
@@ -326,40 +343,37 @@ class ProfileScreen extends StatelessWidget {
             titleText: "Email",
             hintText: "youremail@xmail.com",
             textEditingController:
-            profileScreenController.emailController.value),
+                profileScreenController.emailController.value),
         20.ph,
         editTextField(
             context: context,
             titleText: "Full Name",
             hintText: "William Bennett",
             textEditingController:
-            profileScreenController.fullNameController.value),
+                profileScreenController.fullNameController.value),
         20.ph,
         editTextField(
             context: context,
             titleText: "Street Address",
             hintText: "465 Nolan Causeway Suite 079",
             textEditingController:
-            profileScreenController.addressController.value),
+                profileScreenController.addressController.value),
         20.ph,
         editTextField(
             context: context,
             titleText: "Apt, Suite, Bldg (optional)",
             hintText: "Unit 512",
             textEditingController:
-            profileScreenController.aptSuiteController.value),
+                profileScreenController.aptSuiteController.value),
         20.ph,
         SizedBox(
-          width: MediaQuery
-              .of(context)
-              .size
-              .width * .3,
+          width: MediaQuery.of(context).size.width * .3,
           child: editTextField(
               context: context,
               titleText: "Zip Code",
               hintText: "77017",
               textEditingController:
-              profileScreenController.zipCodeController.value),
+                  profileScreenController.zipCodeController.value),
         ),
         30.ph,
         CommonMethods.saveCancelButtons(
